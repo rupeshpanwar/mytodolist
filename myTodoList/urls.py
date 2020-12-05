4adc0f3f108a925f6c37082f39d4620537304d7a
rupeshpanwar <rupeshpanwar28@gmail.com>
2020-12-05 18:00:34 +0530

    signup signout login page done

--- myTodoList/urls.py
+++ myTodoList/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    #signup page
+    path('signup',views.signupuser,name='signupuser'),
+    path('logout',views.logoutuser,name='logoutuser'),
+    path('login',views.loginuser,name='loginuser'),
+
+    #to do
+    path('',views.home,name='home'),
+    path('current',views.currenttodos,name='currenttodos'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login,logout,authenticate
 
 # Create your views here.
+def home(request):
+      return render(request,'todo/home.html')
+
+def signupuser(request):
+     if request.method == 'GET':
+           return render(request,'todo/signupuser.html',{'form':UserCreationForm()})
+     else:
+        #create new User
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                    user = User.objects.create_user(request.POST['username'],
+                                             password = request.POST['password1']
+                                             )
+                    user.save()
+                    login(request,user)
+                    return redirect('currenttodos')
+            except IntegrityError:
+                           return render(request,
+                                         'todo/signupuser.html',
+                                         {'form':UserCreationForm(),
+                                          'error':'This User already exists, please try different one '
+                                         }
+                                         )
+
+        else:
+            return render(request,
+                          'todo/signupuser.html',
+                          {'form':UserCreationForm(),
+                           'error':'Enter same password'
+                          }
+                          )
+
+def loginuser(request):
+     if request.method == 'GET':
+           return render(request,'todo/loginuser.html',{'form':AuthenticationForm()})
+     else:
+           user = authenticate(request,
+                        username=request.POST['username'],
+                        password=request.POST['password']
+                        )
+           if user is None:
+               return render(request,
+                             'todo/loginuser.html',
+                               {'form':AuthenticationForm(),
+                               'error':'either username or password didnt match'}
+                            )
+           else:
+                    login(request,user)
+                    return redirect('currenttodos')
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
+def currenttodos(request):
+           return render(request,'todo/currenttodos.html')
